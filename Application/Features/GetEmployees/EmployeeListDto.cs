0b6066eaa546e79c3812cbcac0713bfa8a66b49c
JOSE ANGEL LUGO ESPARZA <jose.lugo@mesa.ms>
2026-02-26 08:05:08 -0600

    refactor: correcion  de ortografia

---
+++ Application/Features/GetEmployees/EmployeeListDto.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.Features.GetEmployees
+{
+    public class EmployeeListDto
+    {
+        public int PayRollNumber { get; set; }
+
+        public string FullName { get; set; }
+
+        public string Department {  get; set; }
+
+        public int YearsOfService { get; set; }
+
+        public decimal TotalVacationDays { get; set; }
+    }
+}
