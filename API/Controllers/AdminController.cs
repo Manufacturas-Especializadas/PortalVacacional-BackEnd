687502ed5ba0f650aea317431f6abd199f288ad9
JOSE ANGEL LUGO ESPARZA <jose.lugo@mesa.ms>
2026-02-26 08:05:57 -0600

    feat: endpoint para obtener todos los empleados y sus dias disponibles

--- API/Controllers/AdminController.cs
+++ API/Controllers/AdminController.cs
@@ -1,6 +1,8 @@
 ﻿using Application.Features.Admin;
-using Microsoft.AspNetCore.Http;
+using Application.Features.GetEmployees;
+using Infrastructure.Data;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace API.Controllers
 {
@@ -9,10 +11,40 @@ namespace API.Controllers
     public class AdminController : ControllerBase
     {
         private readonly IEmployeeImportService _importService;
+        private readonly AppDbContext _context;
 
-        public AdminController(IEmployeeImportService importService)
+        public AdminController(IEmployeeImportService importService, AppDbContext context)
         {
             _importService = importService;
+            _context = context;
+        }
+
+        [HttpGet]
+        [Route("employees")]
+        public async Task<IActionResult> GetEmployees()
+        {
+            var currentYear = DateTime.UtcNow.Year;
+
+            var employees = await _context.Users
+                    .Where(u => u.Role.Name == "Employee")
+                     .Select(u => new EmployeeListDto
+                     {
+                         PayRollNumber = u.PayRollNumber,
+                         FullName = u.FullName,
+                         Department = u.EmployeeProfile != null && u.EmployeeProfile.Department != null
+                        ? u.EmployeeProfile.Department.Name
+                        : "Sin departamento",
+
+                                 YearsOfService = u.EmployeeProfile != null && u.EmployeeProfile.HireDate != null
+                        ? currentYear - u.EmployeeProfile.HireDate.Year
+                        : 0,
+
+                         TotalVacationDays = u.VacationBalances
+                            .Sum(v => (decimal)(v.AssignedDays - v.UsedDays))
+                     })
+                    .ToListAsync();
+
+            return Ok(employees);
         }
 
         [HttpPost]
