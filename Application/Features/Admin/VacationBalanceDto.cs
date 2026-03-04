18100ed4b5f101b742cd68a5792c1ae6b624f9fb
JOSE ANGEL LUGO ESPARZA <jose.lugo@mesa.ms>
2026-03-04 15:37:57 -0600

    feat: dto de balance de vacaciones

---
+++ Application/Features/Admin/VacationBalanceDto.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.Features.Admin
+{
+    public class VacationBalanceDto
+    {
+        public int Year { get; set; }
+
+        public int AssignedDays { get; set; }
+    }
+}
