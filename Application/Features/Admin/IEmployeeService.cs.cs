7e1cacc940294be7878a861be478da14a14ac447
JOSE ANGEL LUGO ESPARZA <jose.lugo@mesa.ms>
2026-03-04 15:41:17 -0600

    feat: interfaces para actualizar y eliminar

---
+++ Application/Features/Admin/IEmployeeService.cs.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.Features.Admin
+{
+    public interface IEmployeeService
+    {
+        Task<bool> UpdateEmployeeAsync(UpdateEmployeeDto dto);
+
+        Task<bool> DeleteEmployeeAsync(int payrollNumber);
+    }
+}
