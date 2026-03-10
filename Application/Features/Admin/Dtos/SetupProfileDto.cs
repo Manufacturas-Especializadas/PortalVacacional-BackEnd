d3f7d73ad8407097bde68ad14bca2452af8be9c3
JOSE ANGEL LUGO ESPARZA <jose.lugo@mesa.ms>
2026-03-10 07:13:18 -0600

    feat: dto configuracion de perfil

---
+++ Application/Features/Admin/Dtos/SetupProfileDto.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.Features.Admin.Dtos
+{
+    public class SetupProfileDto
+    {
+        public string Email { get; set; }
+
+        public string NewPassword { get; set; }
+
+        public string ConfirmPassword { get; set; }
+    }
+}
