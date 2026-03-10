d34a3b701246f2971919d04f7f2e167ce92037d4
JOSE ANGEL LUGO ESPARZA <jose.lugo@mesa.ms>
2026-03-10 14:15:05 -0600

    feat: endpoint para la solicitud de vacaciones

--- API/Controllers/EmployeeController.cs
+++ API/Controllers/EmployeeController.cs
@@ -1,4 +1,5 @@
 ﻿using Application.Features.Admin;
+using Application.Features.Employee.Dtos;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using System.Security.Claims;
@@ -25,6 +26,27 @@ namespace API.Controllers
             var data = await _employeeService.GetDashboardDataAsync(userId);
 
             return Ok(data);
+        }
+
+        [HttpPost]
+        [Route("request-vacation")]
+        public async Task<IActionResult> RequestVacation([FromBody] CreateVacationRequestDto dto)
+        {
+            var userId = int.Parse(User.FindFirst(ClaimTypes.NameIdentifier)?.Value!);
+
+            try
+            {
+                var success = await _employeeService.RequestVacationAsync(userId, dto);
+
+                if (success)
+                    return Ok(new { message = "Solicitud enviada con éxito. Tu jefe recibirá una notificación" });
+
+                return BadRequest(new { message = "No se pudo procesar la solicitud" });
+            }
+            catch(Exception ex)
+            {
+                return BadRequest(new { message = ex.Message });
+            }
         }
     }
 }
