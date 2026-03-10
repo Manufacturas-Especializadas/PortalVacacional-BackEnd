c0fb11f831577694b772f2d1194aa2ae4ea9f836
JOSE ANGEL LUGO ESPARZA <jose.lugo@mesa.ms>
2026-03-10 08:36:49 -0600

    feat: vacation dto

---
+++ Application/Features/Employee/Dtos/VacationRequestDto.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.Features.Employee.Dtos
+{
+    public class VacationRequestDto
+    {
+        public int Id { get; set; }
+
+        public string StartDate { get; set; }
+
+        public string EndDate { get; set; }
+
+        public int Days { get; set; }
+
+        public string Status { get; set; }
+    }
+}
