1efd2413ea26c5c8a01b46a04ea21b1bee17d7b0
JOSE ANGEL LUGO ESPARZA <jose.lugo@mesa.ms>
2026-03-10 14:14:42 -0600

    feat: dto para la creacion de solicitud de vacaciones

---
+++ Application/Features/Employee/Dtos/CreateVacationRequestDto.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.Features.Employee.Dtos
+{
+    public class CreateVacationRequestDto
+    {
+        public DateTime StartDate { get; set; }
+
+        public DateTime EndDate { get; set; }
+
+        public int RequestedDays { get; set; }
+    }
+}
