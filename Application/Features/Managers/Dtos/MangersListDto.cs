caeff1f6289a5222294e4889ce252948804ec586
JOSE ANGEL LUGO ESPARZA <jose.lugo@mesa.ms>
2026-03-11 10:18:31 -0600

    feat: dto manager

---
+++ Application/Features/Managers/Dtos/MangersListDto.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.Features.Managers.Dtos
+{
+    public class MangersListDto
+    {
+        public int Id { get; set; }
+
+        public int PayRollNumber { get; set; }
+
+        public string FullName { get; set; }
+
+        public string Department { get; set; }
+
+        public int RoleId { get; set; }
+
+        public DateTime HireDate { get; set; }
+
+        public int YearsOfService { get; set; }
+
+        public decimal TotalVacationDays { get; set; }
+
+        public bool IsActive { get; set; }
+    }
+}
