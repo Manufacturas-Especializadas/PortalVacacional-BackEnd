af9f526afcce8dd3cc2655a05a918c02d57c08b1
JOSE ANGEL LUGO ESPARZA <jose.lugo@mesa.ms>
2026-03-11 10:18:44 -0600

    feat: entidad manager

---
+++ Core/Entities/Manager.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core.Entities
+{
+    public class Manager
+    {
+        public int Id { get; set; }
+
+        public string FullName { get; set; } = null!;
+
+        public string Email { get; set; } = null!;
+
+        public int PayRollNumber { get; set; }
+
+        public int DepartmentId { get; set; }
+
+        public int RoleId { get; set; }
+
+        public bool IsActive { get; set; } = true;
+
+        public DateTime CreatedAt { get; set; }
+
+        public Role Role { get; set; } = null!;
+
+        public Department Department { get; set; } = null!;
+    }
+}
